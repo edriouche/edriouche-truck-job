da0e64c51cd75cced3a51d8d2f1466a942a6840f
Mohammed Edriouche <edriouche@gmail.com>
2026-09-21 08:59:37 +0200

    Use persistent SQLite database in /data

--- docker/start.sh
+++ docker/start.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 set -e
 
-mkdir -p /var/www/html/database
-touch /var/www/html/database/database.sqlite
+mkdir -p /data
+
+export DB_CONNECTION=sqlite
+export DB_DATABASE=/data/database.sqlite
+
+if [ ! -f /data/database.sqlite ]; then
+    cp /var/www/html/database/database.sqlite /data/database.sqlite
+fi
 
 php artisan migrate --force
 
