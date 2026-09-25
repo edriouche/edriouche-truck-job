604a39cab5accfe5f311b309b439265deaaf9ce1
Mohammed Edriouche <edriouche@gmail.com>
2026-09-25 13:22:16 +0100

    Revert "Fix gallery storage link"
    This reverts commit 963691939a6ccdc5a030596ea2d874f5b97451da.

--- docker/start.sh
+++ docker/start.sh
@@ -9,8 +9,6 @@ export DB_CONNECTION=sqlite
 export DB_DATABASE=/data/database.sqlite
 export GALLERY_STORAGE_PATH=/data/gallery
 mkdir -p /data/gallery
-mkdir -p /var/www/html/public/storage
-ln -sfn /data/gallery /var/www/html/public/storage/gallery
 
 if [ ! -f /data/database.sqlite ]; then
     cp /var/www/html/database/database.sqlite /data/database.sqlite
